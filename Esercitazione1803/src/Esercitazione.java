d569c29f38881a59d983d71a42f1bcc64522da71
toto <antoniolabate19@gmail.com>
2022-03-18 15:34:54 +0100

    esercizio costruisciArray

--- Esercitazione1803/src/Esercitazione.java
+++ Esercitazione1803/src/Esercitazione.java
@@ -32,4 +32,30 @@ public class Esercitazione {
         }
         return true;
     }
+
+    /* si scriva un metodo costrusciArray che riceve in unpout un array di interi v1
+    e restituisce un array di interi v2 di pari lunghezza il cui generico elemento i è così ottenuto:
+    - se la media degli elementi di v1 con indice maggiore o uguale a i è maggiore o uguale a v1[i], allora v2[i] è uguale a tale media;
+    - altrimenti v2[i] è uguale alla differenza tra la somma degli elementi a sinistra di v1[i] e la somma degli degli elementi alla destra di v1[i].
+    Ovviamente se non ci sono elementi a sx o a dx la somma vale 0. */
+    public static int[] costrusciArray(int[] v1) {
+        int[] v2 = new int[v1.length];
+        for (int i = 0; i < v1.length; i++) {
+            int sommaDx = 0;
+            for (int j = 0; j < i; j++){
+                sommaDx += v1[j];
+            }
+            int media = sommaDx/(v1.length - i);
+            if (media >= v1[i]) {
+                v2[i] = media;
+            } else {
+                int sommaSx = 0;
+                for (int k = 0; k < i; i++) {
+                    sommaSx += v2[k];
+                }
+                v2[i] = sommaDx - sommaSx;
+            }
+        }
+        return v2;
+    }
 }
