726006e46d82e6f0143b93a5e0763af375e55c62
toto <antoniolabate19@gmail.com>
2022-03-25 16:31:17 +0100

    class+package conticorrenti

--- Esercitazione2503/src/conticorrenti/ContoCorrente.java
+++ Esercitazione2503/src/conticorrenti/ContoCorrente.java
@@ -1,6 +1,8 @@
 package conticorrenti;
 import java.util.*;
+
 public class ContoCorrente {
+
 	private String IBAN; // key element - attributo identificatore
 	/* non possono esistere due oggetti diversi con lo stesso IBAN
 	 * se ci sono due oggetti con lo stesso IBAN e diversi valori per altri attributi ci dice che c'è un errore
@@ -9,4 +11,39 @@ public class ContoCorrente {
 	private String intestatario;
 	private ArrayList<Movimento> movimenti;
 	private float fido;
+
+	public ContoCorrente(String IBAN, String intestatario) {
+		this.IBAN = IBAN;
+		this.intestatario = intestatario;
+		this.movimenti = new ArrayList<>();
+		this.fido = 0;
+	}
+	
+	public ContoCorrente(ContoCorrente c) {
+		this.IBAN = c.IBAN;
+		this.intestatario = c.intestatario;
+		this.movimenti = new ArrayList<>(c.movimenti);		
+		this.fido = c.fido;
+	}	
+
+	public void setFido(float fido) {
+		this.fido = fido;
+	}
+	
+	public void aggiungiMovimento(Movimento m) {
+		movimenti.add(m);
+	}
+
+	public float getSaldoContabile() {
+		float somma = 0;
+		for (Movimento m : movimenti) {
+			somma += m.getValore();
+		}
+		return somma;
+	}
+	
+	public float getSaldoDisponibile() {
+		return getSaldoContabile() + this.fido;
+	}
+
 }
