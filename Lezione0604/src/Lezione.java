829209da5e648db699f3bb213d7a7e4284f9c0ce
toto <antoniolabate19@gmail.com>
2022-04-06 16:10:41 +0200

    refactored test file

--- Lezione0604/src/Lezione.java
+++ Lezione0604/src/Lezione.java
@@ -5,6 +5,22 @@ import java.util.*;
 public class Lezione {
 
 	public static void main(String[] args) {
+		ordinamento();
+	}
+
+	
+	public static void automa() {
+		Automa a = new Automa();
+		String s = "abbbbcc"; // pattern: 1 a, almeno 1 b, 2 c. Con altri pattern non viene riconosciuta.
+		boolean riconosciuta = a.riconosci(s);
+		if (riconosciuta) {
+			Terminale.stampa("Stringa " + s + " riconosciuta");
+		} else {
+			Terminale.stampa("Stringa " + s + " non riconosciuta");
+		}
+	}
+	
+	public static void testRazionali() {
 		ArrayList<Razionale> lista = new ArrayList<>();
 		for (int n = 0; n <= 10; n++) {
 			lista.add(new Razionale(n, 10));
@@ -23,18 +39,18 @@ public class Lezione {
 		}
 		
 		Terminale.stampa(lista);
-
 	}
-
 	
-	public static void automa() {
-		Automa a = new Automa();
-		String s = "abbbbcc"; // pattern: 1 a, almeno 1 b, 2 c. Con altri pattern non viene riconosciuta.
-		boolean riconosciuta = a.riconosci(s);
-		if (riconosciuta) {
-			Terminale.stampa("Stringa " + s + " riconosciuta");
-		} else {
-			Terminale.stampa("Stringa " + s + " non riconosciuta");
-		}
+	public static void ordinamento() {
+		ArrayList<Razionale> lista = new ArrayList<>();
+		lista.add(new Razionale(1, 2));
+		lista.add(new Razionale(1, 3));
+		lista.add(new Razionale(1, 4));
+		Terminale.stampa(lista);
+		
+		Collections.sort(lista);
+		Terminale.stampa(lista);
+
 	}
+	
 }
