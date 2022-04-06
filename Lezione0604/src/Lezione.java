9fadb2734d2548260e46b40d70798447a072ea85
toto <antoniolabate19@gmail.com>
2022-04-06 15:29:36 +0200

    merge file lezione con quello del prof

--- Lezione0604/src/Lezione.java
+++ Lezione0604/src/Lezione.java
@@ -1,8 +1,19 @@
 import terminale.Terminale;
+import razionali.*;
+import java.util.*;
 
 public class Lezione {
 
 	public static void main(String[] args) {
+		ArrayList<Razionale> lista = new ArrayList<>();
+		for (int n = 0; n <= 10; n++) {
+			lista.add(new Razionale(n, 10));
+		}
+		Terminale.stampa(lista);
+	}
+
+	
+	public static void automa() {
 		Automa a = new Automa();
 		String s = "abbbbcc"; // pattern: 1 a, almeno 1 b, 2 c. Con altri pattern non viene riconosciuta.
 		boolean riconosciuta = a.riconosci(s);
@@ -12,5 +23,4 @@ public class Lezione {
 			Terminale.stampa("Stringa " + s + " non riconosciuta");
 		}
 	}
-
 }
--- Lezione0604/src/razionali/Razionale.java
+++ Lezione0604/src/razionali/Razionale.java
@@ -1,52 +1,63 @@
 package razionali;
 
-public class Razionale 
-{	private int numeratore;
-	private int denominatore;
+public class Razionale { // mutabile
+	private int numeratore;
+	private int denominatore; 
 	
-	public Razionale(int numeratore, int denominatore)
-	{	this.numeratore = numeratore;
+	
+	public Razionale (int numeratore, int denominatore) {
+		this.numeratore = numeratore;
 		this.denominatore = denominatore;
 		
-		if(this.denominatore < 0)
-		{	this.numeratore *= -1;
-			this.denominatore *= -1;			
+		if (this.denominatore < 0) {
+			this.numeratore *= -1;
+			this.denominatore *= -1; // sempre positivo
 		}
 		
-		semplifica();	
+		semplifica(); // sempre ridotto ai minimi termini
 	}
 	
-	public Razionale(int n)
-	{	numeratore = n;
-		denominatore = 1;
-		//ALTERNATIVA:
-		//this(n,1);		
+	public Razionale (int n) {
+		this.numeratore = n;
+		this.denominatore = 1;
+		// alternativa:
+		// this(n, 1); // chiama l'altro costruttore
 	}
 	
-	public Razionale(Razionale r)
-	{	numeratore = r.numeratore;
-		denominatore = r.denominatore;
-		//ALTERNATIVA:
-		//this(r.numeratore,r.denominatore);
+	public Razionale(Razionale r) {
+		this.numeratore = r.numeratore;
+		this.denominatore = r.denominatore;
+		// this(r.numeratore, r.denominatore);
 	}
 	
-	public Razionale()
-	{	numeratore = 0;
-		denominatore = 1;
-		//ALTERNATIVA:
-		//this(0,1);
-		//ALTERNATIVA:
-		//this(0);
+	public Razionale() {
+		this.numeratore = 0;
+		this.denominatore = 1;
+		// this(0, 1);
+		// this(0);
 	}
 	
-	private void semplifica()
-	{	//DA FARE		
+	private void semplifica () {
+		if (numeratore == 0) {
+			denominatore = 1;
+		} else {
+			int mcd = massimoComunDivisore(Math.abs(numeratore), denominatore);
+			numeratore /= mcd;
+			denominatore /= mcd;
+		}
 	}
-
+	
 	private int massimoComunDivisore(int a, int b)
 	{	for(int v = Math.min(a, b); ;v--)
 			if(a % v == 0 && b % v == 0)
 				return v;	
+	}
+	
+	public void moltiplica(Razionale r) {
+		this.numeratore *= r.numeratore;
+		this.denominatore *= r.denominatore;
+		
+		semplifica();
 	}
 	
 	public String toString()
@@ -54,15 +65,7 @@ public class Razionale
 			return ""+numeratore;
 		return numeratore+"/"+denominatore;
 	}
+
 	
-	public boolean equals(Object o)
-	{	if(o == null)
-			return false;
-		if(o == this)
-			return true;
-		if(!(o instanceof Razionale))
-			return false;
-		Razionale r = (Razionale)o;
-		return numeratore == r.numeratore && denominatore == r.denominatore;
-	}
+	
 }
