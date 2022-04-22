ac0e4454ab6fdb7f54c3de0a3746090ec5f8d79a
toto <antoniolabate19@gmail.com>
2022-04-22 15:33:23 +0200

    added more methods

--- Lezione1204/src/listeconcatenate/ListaConcatenataInt.java
+++ Lezione1204/src/listeconcatenate/ListaConcatenataInt.java
@@ -112,7 +112,84 @@ public class ListaConcatenataInt
 	}
 	
 	public boolean equals(Object o) {
-		//DA FARE
-		return false;
+		if (o == null) {
+			return false;
+		}
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof ListaConcatenataInt)) {
+			return false;
+		}
+		
+		ListaConcatenataInt l = (ListaConcatenataInt) o;
+		if (lunghezza != l.lunghezza) {
+			return false;
+		}
+		NodoInt corrente = testa;
+		NodoInt correnteL = l.testa;
+		while (corrente != null) {
+			if (corrente.getInfo() != correnteL.getInfo()) {
+				return false;
+			}
+			corrente = corrente.getSuccessivo();
+			correnteL = correnteL.getSuccessivo();
+		}
+		return true;
 	}
+	
+	public ListaConcatenataInt listaInvertita() {
+		ListaConcatenataInt ret = new ListaConcatenataInt();
+		for (NodoInt corrente = testa; corrente != null; corrente = corrente.getSuccessivo()) {
+			ret.aggiungiInTesta(corrente.getInfo());
+		}
+		return ret;
+	}
+	
+	public ArrayList<Integer> adArrayList() {
+		ArrayList<Integer> ret = new ArrayList<>();
+		for (NodoInt corrente = testa; corrente != null; corrente = corrente.getSuccessivo()) {
+			ret.add(corrente.getInfo());
+		}
+		return ret;
+	}
+	
+	public int[] adArray() {
+		if (eVuota()) throw new EccezioneListaVuota();
+		int[] ret = new int[lunghezza];
+		int posLibera = 0;
+		for (NodoInt corrente = testa; corrente != null; corrente = corrente.getSuccessivo()) {
+			ret[posLibera] = corrente.getInfo();
+			posLibera++;
+		}
+		return ret;
+	}
+	
+	public void rimuoviTesta() {
+		if (eVuota()) {
+			throw new EccezioneListaVuota();
+		}
+		if (lunghezza == 1) {
+			svuota();
+			return;
+		}
+		testa = testa.getSuccessivo();
+		lunghezza--;
+	}
+		
+	public int indiceDi(int info) {
+		int pos = 0;
+		for (NodoInt corrente = testa; corrente != null; corrente = corrente.getSuccessivo()) {
+			if (corrente.haInfo(info)) {
+				return pos;
+			}
+			pos++;
+		}
+		return -1;
+	}
+	
+	public boolean contiene(int info) {
+		return indiceDi(info) != -1;
+	}
+	
 }
