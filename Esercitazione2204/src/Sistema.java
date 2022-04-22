79fdba9343fa8f16c99dd5b0bf848ffa0e5eae79
toto <antoniolabate19@gmail.com>
2022-04-22 16:27:28 +0200

    inizio esercizio

--- Esercitazione2204/src/Sistema.java
+++ Esercitazione2204/src/Sistema.java
@@ -1,4 +1,26 @@
+import java.util.*;
 
 public class Sistema {
-
+	
+	private LinkedList<Tecnico> tecnici;
+	private LinkedList<Intervento> interventi;
+	
+	public Sistema(LinkedList<Tecnico> tecnici, LinkedList<Intervento> interventi) {
+		this.tecnici = new LinkedList<>(tecnici);
+		this.interventi = new LinkedList<>(interventi);
+	}
+	
+	public Tecnico tecnicoMax() {
+		// TODO
+		return null;
+	}
+	public int costoComplessivo(Tecnico t) {
+		int sommaDurateInterventi = 0;
+		for (Intervento i : interventi) {
+			if (i.getNomeTecnico().equals(t.getNome())) {
+				sommaDurateInterventi += i.getDurata();
+			}
+		}
+		return sommaDurataInterventi * t.getCostoOrario();
+	}
 }
