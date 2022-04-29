852aa2040b0b0dc9fc26024773d6138afcb1bce5
toto <antoniolabate19@gmail.com>
2022-04-29 12:57:23 +0200

    modifiche integrate

--- Esercitazione2704/src/autostrade/Citta.java
+++ Esercitazione2704/src/autostrade/Citta.java
@@ -2,43 +2,35 @@ package autostrade;
 
 public class Citta {
 	
-	private String nome, provincia, regione;
+	private String nome, provincia,regione;
 	
 	public Citta(String nome, String provincia, String regione) {
 		this.nome = nome;
 		this.provincia = provincia;
 		this.regione = regione;
+		
 	}
 	
 	public String getNome() {
 		return nome;
 	}
-	
 	public String getProvincia() {
 		return provincia;
 	}
-	
 	public String getRegione() {
 		return regione;
 	}
 	
 	public String toString() {
-		return nome + " (" + provincia + ") in " + regione;
+		return nome+" ("+provincia+") in "+regione;
 	}
 	
 	public boolean equals(Object o) {
-		if (o == null) {
-			return false;
-		}
-		if (this == o) {
-			return true;
-		}
-		if (!(o instanceof Citta)) {
-			return false;
-		}
-		
+		if(o==null) return false;
+		if(o==this) return true;
+		if(!(o instanceof Citta)) return false;
 		Citta c = (Citta) o;
 		return this.nome.equals(c.nome) && this.provincia.equals(c.provincia);
 	}
-	
+
 }
--- Esercitazione2704/src/autostrade/Tratta.java
+++ Esercitazione2704/src/autostrade/Tratta.java
@@ -9,7 +9,49 @@ public class Tratta {
 	public Tratta(String codice, String nome, Citta cittaPartenza, Citta cittaDestinazione, double distanza) {
 		this.codice = codice;
 		this.nome = nome;
-		// TODO pc scarico
+		this.cittaPartenza = cittaPartenza; // senza protective copy perché genera oggetti immutabili
+		this.cittaDestinazione = cittaDestinazione;
+		//this.cittaPartenza = new Citta(cittaPartenza.getNome(), cittaPartenza.getProvincia(), cittaPartenza.getRegione());
+		//this.cittaDestinazione = new Citta(cittaDestinazione.getNome(), cittaDestinazione.getProvincia(), cittaPartenza.getRegione());
+		this.distanza = distanza;
+	}
+	
+	public String getCodice() {
+		return codice;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public Citta getCittaPartenza() {
+		return cittaPartenza;
+	}
+	
+	public Citta getCittaDestinazione() {
+		return cittaDestinazione;
+	}
+	
+	public double getDistanza() {
+		return distanza;
+	}
+	
+	public String toString() {
+		return "Tratta con codice " + codice + " tra " + cittaPartenza.toString() + " e " + cittaDestinazione.toString();
+	}
+	
+	public boolean equals(Object o) {
+		if (o == null) {
+			return false;
+		}
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof Tratta)) {
+			return false;
+		}
 		
+		Tratta t = (Tratta) o;
+		return this.codice.equals(t.codice);
 	}
 }
