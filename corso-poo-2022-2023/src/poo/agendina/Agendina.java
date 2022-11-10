a3c6571521cb3045480b01ceb7f6275a0b0c7fca
toto <antoniolabate19@gmail.com>
2022-11-10 22:18:00 +0100

    added default methods to interface

--- corso-poo-2022-2023/src/poo/agendina/Agendina.java
+++ corso-poo-2022-2023/src/poo/agendina/Agendina.java
@@ -1,19 +1,64 @@
 package poo.agendina;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public interface Agendina extends Iterable<Nominativo> {
 
-	public int size();
+	default public int size() {
+		Iterator<Nominativo> it = this.iterator();
+		int cont = 0;
+		while (it.hasNext()) {
+			it.next();
+			cont++;
+		}
+		return cont;
+	}
 	
-	public void svuota();
+	default public void svuota() {
+		Iterator<Nominativo> it = this.iterator();
+		while (it.hasNext()) {
+			it.next();
+			it.remove();
+		} 
+	}
+	
 	public void aggiungi(Nominativo n);
-	public void rimuovi(Nominativo n);
 	
-	public Nominativo cerca(Nominativo n);
-	public Nominativo cerca(String prefisso, String telefono);
+	default public void rimuovi(Nominativo n) {
+		Iterator<Nominativo> it = this.iterator();
+		while (it.hasNext()) {
+			it.next();
+			if (it.equals(n)) {
+				it.remove();
+			}
+		}
+	}
+	
+	default public Nominativo cerca(Nominativo n) {
+		Iterator<Nominativo> it = this.iterator();
+		while (it.hasNext()) {
+			Nominativo ret = it.next();
+			if (ret.equals(n)) {
+				return ret;
+			}
+		}
+		return null;
+	}
+	
+	default public Nominativo cerca(String prefisso, String telefono) {
+		Iterator<Nominativo> it = this.iterator();
+		while (it.hasNext()) {
+			Nominativo ret = it.next();
+			if (ret.getPrefisso().equals(prefisso) &&
+					ret.getNumero().equals(telefono)) {
+				return ret;
+			}
+		}
+		return null;
+	}
 	
 	public void salva(String nomeFile) throws IOException;
-	public void ripristina(String nomeFile);
+	public void ripristina(String nomeFile) throws IOException;
 	
 }
