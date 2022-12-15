52e976d525fbdf84c990039f22e4b69e5e968275
toto <antoniolabate19@gmail.com>
2022-12-15 15:14:27 +0100

    implementazione StringaIF

--- corso-poo-2022-2023/src/esercitazioni/stringhe/StringaConcatenata.java
+++ corso-poo-2022-2023/src/esercitazioni/stringhe/StringaConcatenata.java
@@ -2,111 +2,103 @@ package esercitazioni.stringhe;
 
 import java.util.*;
 
-public class StringaConcatenata extends StringaAbstract{
-	private static class Nodo{
+import javax.management.RuntimeErrorException;
+
+public class StringaConcatenata extends StringaAbstract {
+
+	private static class Nodo {
 		char info;
 		Nodo next;
 	}
-	private Nodo testa=null, coda=null;
-
-	public Iterator<Character> iterator(){
-		return new Iteratore();
-	}
 	
-	protected StringaIF newInstanceStringa(){
+	private Nodo testa = null, coda = null;
+
+	@Override
+	public void append(char c) {
+		Nodo n = new Nodo();
+		n.info = c;
+		if (testa == null) {
+			testa = n;
+			coda = n;
+		} else {
+			coda.next = n;
+			coda = n;
+		}
+	}
+
+	@Override
+	public StringaIF copy() {
+		StringaIF res = newInstanceStringa();
+		res.append(this); // boh non ho mica capito 
+		/*
+		Nodo n = testa;
+		while (n != null) {
+			res.append(n.info);
+		}*/
+		return res;
+	}
+
+	@Override
+	public void setCharAt(int pos, char c) { // inefficiente
+		if (pos < 0 || this.length() < pos) throw new IllegalArgumentException();
+		Nodo n = testa;
+
+		for (int i = 0; i <= pos; i++) {
+			n = n.next;
+		}
+		n.info = c;
+	}
+
+	@Override
+	protected StringaIF newInstanceStringa() {
 		return new StringaConcatenata();
 	}
-
-	public void setCharAt( int pos, char c ){
-		if( pos<0 || pos>=this.length() )
-			throw new IndexOutOfBoundsException();
-		int i=0;
-		Nodo n=testa;
-		for( ; i<pos; i++, n=n.next );
-		n.info=c;
+	
+	@Override
+	public Iterator<Character> iterator() {
+		return new MioIteratore();
 	}
 	
-	public void append( char c ){
-		Nodo n=new Nodo(); 
-		n.info=c; 
-		n.next=null;
-		if( testa==null ) 
-			testa=n;
-		else 
-			coda.next=n;
-		coda=n;
-	}
-	
-	public StringaIF copy(){
-		StringaIF sc=new StringaConcatenata();
-		sc.append( this );
-		return sc;
-	}
+	private class MioIteratore implements Iterator<Character> {
 
-	//i metodi che seguono sono ridefiniti per scopi dimostrativi
-	public StringaIF substring( int da ){
-		if( da<0 || da>=this.length() )
-			throw new IndexOutOfBoundsException();
-		StringaIF sub=new StringaConcatenata();
-		Nodo cor=testa;
-		int i=0;
-		for( ; i<this.length(); i++, cor=cor.next ){
-			if( i>=da ) sub.append( cor.info );
-		}
-		return sub;
-	}
-
-	public StringaIF substring( int da, int a ){
-		if( da<0 || da>=this.length() || a<0 || a>=this.length() || da>a )
-			throw new IndexOutOfBoundsException();
-		StringaIF sub=new StringaConcatenata();
-		Nodo cor=testa;
-		int i=0;
-		for( ; i<this.length(); i++, cor=cor.next ){
-			if( i>=da && i<a ) sub.append( cor.info );
-		}
-		return sub;
-	}
-
-	private class Iteratore implements Iterator<Character>{
-
-		Nodo pre=null, cor=null, suc=testa;
-		boolean rimovibile = false; 
+		Nodo prec = null, corr = null, succ = testa; // modello a "puntatore dietro il primo elemento"
+		boolean rimuovibile = false;
+		
 		@Override
 		public boolean hasNext() {
-			return suc!=null;
+			return succ != null;
 		}
 
 		@Override
 		public Character next() {
-			if(!hasNext()) throw new NoSuchElementException();
-			pre=cor;
-			cor=suc;
-			suc=suc.next;
-			rimovibile=true;
-			return cor.info;
+			if (!hasNext()) throw new RuntimeException();
+			prec = corr;
+			corr = succ;
+			succ = succ.next;
+			rimuovibile = true;
+			return corr.info;
 		}
 		
+		@Override
 		public void remove() {
-			if(!rimovibile) throw new IllegalStateException();
-			rimovibile=false;
-			if(cor==testa) {
-				testa=testa.next;
-				cor.next=null;
-				cor=pre;
-				if(testa==null)coda=null;
-			} else {
-				if(cor==coda) {
-					pre.next=null;
-					coda=pre;
-				} else {
-					cor.next=null;
-					pre.next=suc;
+			if (!rimuovibile) throw new RuntimeException();
+			rimuovibile = false;
+			
+			if (corr == testa) {
+				testa = corr.next;
+				if (testa == null) {
+					coda = null;
 				}
-				cor=pre;
+			} else {
+				if (corr == coda) {
+					coda = prec;
+					prec.next = null;
+				} else {
+					prec.next = corr.next;
+				}
 			}
+			corr = prec;
 		}
-		
 	}
 
-}//StringaConcatenata
+}
