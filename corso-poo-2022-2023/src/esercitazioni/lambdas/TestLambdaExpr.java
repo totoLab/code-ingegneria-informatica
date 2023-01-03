7575d5679ad70f5bb1dabaca61ee9c1af012f3c2
toto <antoniolabate19@gmail.com>
2023-01-03 12:29:29 +0100

    fixed errors, removed uncompleted methods

--- corso-poo-2022-2023/src/esercitazioni/lambdas/TestLambdaExpr.java
+++ corso-poo-2022-2023/src/esercitazioni/lambdas/TestLambdaExpr.java
@@ -1,4 +1,8 @@
+package esercitazioni.lambdas;
+
 import java.util.function.*;
+import java.util.*;
+import java.util.stream.*;
 
 public class TestLambdaExpr {
 	/*
@@ -19,7 +23,7 @@ public class TestLambdaExpr {
 	
 		List<String> ret = new LinkedList<>();
 		for (String s : l) {
-			ret.addLast(reverse(s));
+			ret.add(reverse(s));
 		}
 		return ret;
 	}
@@ -27,7 +31,7 @@ public class TestLambdaExpr {
 	public String reverse(String s) {
 		StringBuilder sb = new StringBuilder();
 		for (int i = s.length() - 1; i >= 0; i--) {
-			sb.append(s.chatAt(i));
+			sb.append(s.charAt(i));
 		}
 		return sb.toString();
 	}
@@ -41,31 +45,19 @@ public class TestLambdaExpr {
 	}
 	
 	public boolean filterElem(String s) {
-		return !s.upperCase().contains("E");
+		return !s.toUpperCase().contains("E");
 	}
 	
 	public boolean lambdaFind(String s, List<String> l) {
 		return l.stream().anyMatch(elem -> {
-			return elem.equals(s));
-		})
+			return elem.equals(s);
+		});
 	}
 	
 	public void lambdaPrint(List<String> l) {
-		return l.stream().forEach(elem -> {
+		l.stream().forEach(elem -> {
 			System.out.println(elem);
-		})
+		});
 	}
 	
-	public void lambdaShift(List<Integer> l) {
-		return l.stream().peek(elem -> {
-			return elem + 5;
-		})
-	}
-	
-	public static void main(String[] args) {
-		List<String> l1 = new LinkedList<>();
-		l1.add("TEST");
-		TestLambdaExpr test = new TestLambdaExpr();
-		test.reverseEachString(l1);
-	}
 }
