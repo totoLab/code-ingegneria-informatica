1b34bb55bb3369319e0eaddda93dde182c2354a8
toto <antoniolabate19@gmail.com>
2023-03-08 11:14:11 +0100

    bubble sorts

--- algoritmi/2023-03-08/sorting.py
+++ algoritmi/2023-03-08/sorting.py
@@ -14,3 +14,41 @@ def selection_sort(V):
             if V[j] < V[minp]:
                 minp = j
         V[minp], V[i] = V[i], V[minp]
+
+def bubble_sort(V):
+    n = len(V) - 1
+    for i in range(n):
+        for j in range(n - i):
+            if V[j + 1] < V[j]:
+                V[j + 1], V[j] = V[j], V[j + 1]
+
+def bubble_sort_optimized(V):
+    n = len(V) - 1
+    for i in range(n):
+        swap = False
+        for j in range(n - i):
+            if V[j + 1] < V[j]:
+                V[j + 1], V[j] = V[j], V[j + 1]
+                swap = True
+        if not swap:
+            break
+
+def bubble_sort_optimized_w(V):
+    n = len(V) - 1
+    i = 0
+    swap = True
+    while i < n and swap:
+        swap = False
+        for j in range(n - i):
+            if V[j + 1] < V[j]:
+                V[j + 1], V[j] = V[j], V[j + 1]
+                swap = True
+        i += 1
+
+V = [5, 7, 2, 9, 6, 12, 4, 11, 1, 3]
+# insertion_sort(V)
+# selection_sort(V)
+# bubble_sort(V)
+# bubble_sort_optimized(V)
+# bubble_sort_optimized_w(V)
+print(V)
