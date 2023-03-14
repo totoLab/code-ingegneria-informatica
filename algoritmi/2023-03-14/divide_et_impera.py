4fac73af4d52a2d2c7a15d4782da0226a555e265
toto <antoniolabate19@gmail.com>
2023-03-14 13:21:03 +0100

    mergesort

--- algoritmi/2023-03-14/divide_et_impera.py
+++ algoritmi/2023-03-14/divide_et_impera.py
@@ -9,3 +9,33 @@ def binary_search(V, x, first, last):
 			return binary_search(V, x, first, middle - 1)
 		else:
 			return binary_search(V, x, middle + 1, last)
+
+
+def merge_sort(A, first, last):
+	if first < last:
+		middle = (first + last) // 2
+		merge_sort(A, first, middle)
+		merge_sort(A, middle + 1, last)
+		merge(A, first, middle, last)
+
+def merge(A, first, middle, last):
+	i, j = first, middle + 1
+	C = []
+	while i <= middle and j <= last:
+		if A[i] <= A[j]:
+			C.append(A[i])
+			i += 1
+		else:
+			C.append(A[j])
+			j += 1
+
+	while i <= middle:
+		C.append(A[i])
+		i += 1
+
+	while j <= last:
+		C.append(A[j])
+		j += 1
+
+	for i in range(len(C)):
+		A[first + i] = C[i]
