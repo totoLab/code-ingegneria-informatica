4a01280b911bb99380aba37e765a4289a132d595
toto <antoniolabate19@gmail.com>
2023-03-17 10:17:34 +0100

    test

--- algoritmi/2023-03-17/main.py
+++ algoritmi/2023-03-17/main.py
@@ -1,13 +1,10 @@
 import PQ
 
-def insertion_test(test_list):
-    queue = PQ.create()
-    for x in test_list:
-        PQ.insert(queue, x)
-    return queue
+def heap_sort(A):
+    Q = PQ.create_from_list(A)
+    for i in range(len(A)):
+        A[i] = PQ.delete_min(Q)
 
-test_values = [5, 10, 16, 15, 27, 22, 25, 20, 17]
-queue = insertion_test(test_values)
-print(queue)
-PQ.delete_min(queue)
-print(queue)
+V  = [5,7,2,8,4,9,6,1,3,7]
+heap_sort(V)
+print(V)
