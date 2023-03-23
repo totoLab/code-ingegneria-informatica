06936247593708fa21f0614260e081356ef77d80
toto <antoniolabate19@gmail.com>
2023-03-23 23:43:29 +0100

    esercitazione 21-03-2023

---
+++ sistemiOperativi/src/threadLifeCicle/ElementoSella.java
@@ -0,0 +1,11 @@
+package threadLifeCicle;
+
+public class ElementoSella extends Thread {
+	// esercizio 2.3
+	// individua se esiste un elemento che è conteporaneamente max riga e min col
+	// data una matrice n x m si utilizzino n thread per determinare il massimo e m per i minimi
+	// main con matrice di test e stampa la posizione del primo trovato
+	
+	// variante: trovare TUTTI i punti di sella
+	
+}
