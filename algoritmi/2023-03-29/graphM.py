dc32f455e03b254f2d7c5d6fc4954e0f8384e168
toto <antoniolabate19@gmail.com>
2023-03-31 08:50:13 +0200

    new methods and refactoring

--- algoritmi/2023-03-29/graphM.py
+++ algoritmi/2023-03-29/graphM.py
@@ -3,11 +3,28 @@
 def createGraph(n): # Theta(n^2)
     return [[0] * n] * n
 
-def addEdge(G, x, y): # Theta(1)
-    G[x][y] = 1
+def copyGraph(G):
+    C = []
+    for i in range(G):
+        C.append([])
+        for j in G[i]:
+            C[i].append(j)
+    return C
 
-def deleteEdge(G, x, y): # Theta(1)
-    G[x][y] = 0
+def size(G):
+    return len(G)
+
+def nodes(G):
+    C = []
+    for i in range(size(G)):
+        C.append(i)
+    return C
+
+def insertEdge(G, i, j): # Theta(1)
+    G[i][j] = 1
+
+def deleteEdge(G, i, j): # Theta(1)
+    G[i][j] = 0
 
 def isEdge(G, x, y): # Theta(1)
     return G[x][y] == 1
@@ -18,3 +35,7 @@ def neighbours(G, i): # O(n)
         if G[i][j] == 1:
             C.append(G[i][j])
     return C
+
+def printGraph(G):
+    for i in range(size(G)):
+        print(G[i])
