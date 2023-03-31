e2cfe26d542b296352c7158beb23d622a8bc17b1
toto <antoniolabate19@gmail.com>
2023-03-31 09:00:01 +0200

    new methods and refactoring

--- algoritmi/2023-03-29/graphL.py
+++ algoritmi/2023-03-29/graphL.py
@@ -3,10 +3,26 @@
 def createGraph(n):  # Theta(n)
     return [[]] * n
 
+def copyGraph(G):   # Theta(m + n) = Theta(m) 
+    C = createGraph(size(G))
+    for i in range(size(G)):
+        for k in G[i]:
+            C[i].append(k)
+    return C
+
+def size(G):
+    return len(G)
+
+def nodes(G): # non necessaria, i nodi sono uno per ogni indice della lista di adiacenza
+    C = []
+    for i in range(size(G)):
+        C.append(i)
+    return C
+
 def isEdge(G, i, j): # O(n)
     return j in G[i]
 
-def addEdge(G, i, j): # Theta(1)
+def insertEdge(G, i, j): # Theta(1)
     if not isEdge(G, i, j):
         G[i].append(j)
 
@@ -14,4 +30,4 @@ def deleteEdge(G, i, j): # O(n)
     return G[i]
 
 def neighbours(G, i): # O(n)
-    return G[i][:]
+    return G[i][:] # entire list's copy
