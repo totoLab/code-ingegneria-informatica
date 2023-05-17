04f7732c37ff9c597507bdd89f84b2c30d3c9260
toto <antoniolabate19@gmail.com>
2023-05-17 20:58:26 +0200

    traccia luglio 2022

---
+++ algoritmi/tracce/202207/main.py
@@ -0,0 +1,33 @@
+'''
+traccia: verifica se esiste almeno un nodo al livello l in T
+        e la somma dei valori dei al livello s è maggiore di s 
+return boolean
+'''
+import tree as t
+
+def livelloSomma(T, l, s):
+    return livelloSommaR(T, l, s, 0, 0)
+
+def livelloSommaR(T, l, s, distance, somma):
+    if l == distance:
+        somma += t.value(T)
+        if somma > s:
+            return True # assumo valori interi positivi
+    elif distance < l:
+        left = right = False
+        if not t.empty(t.right(T)):
+            right = livelloSommaR(t.right(T), l, s, distance + 1, somma)
+        if not t.empty(t.left(T)):
+            left = livelloSommaR(t.left(T), l, s, distance + 1, somma)
+        return right or left
+    return False
+
+A = t.createTree()
+s = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for x in s:
+    print(A)
+    t.insert(A, x)
+    print(A)
+
+
+print(livelloSomma(A, 2, 4))
