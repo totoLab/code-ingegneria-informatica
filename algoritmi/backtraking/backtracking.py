fdefbed9fa33e1d5988c5e67a161a56ac93e2bad
toto <antoniolabate19@gmail.com>
2023-05-23 12:05:27 +0200

    codice inizio lezione 2023-05-23

--- algoritmi/backtraking/backtracking.py
+++ algoritmi/backtraking/backtracking.py
@@ -1,16 +1,14 @@
 # pseudo codice
+import graphM as g
 
 def mapColoring(G, node, colors, coloring):
-    if node >= len(G):
-        return True
-
     for c in colors:
         if check(G, node, c, coloring):
             coloring[node] = c
-            if mapColoring(G, next_node(node), colors, coloring):
+            if mapColoring(G, g.next_node(G, node), colors, coloring):
                 return True
             else:
-                coloring[node] = " "
+                coloring[node] = "   "
     return False
 
 def check(G, node, color, coloring):
