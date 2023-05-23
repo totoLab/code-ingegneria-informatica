48b799c5b27e5403e214b1500c87b97f1bde7c28
toto <antoniolabate19@gmail.com>
2023-05-23 10:23:45 +0200

    eliminated positive integers assumption moving checking of result in entrypoint

--- algoritmi/tracce/202207/main.py
+++ algoritmi/tracce/202207/main.py
@@ -6,21 +6,19 @@ return boolean
 import tree as t
 
 def livelloSomma(T, l, s):
-    return livelloSommaR(T, l, s, 0, 0)
+    return livelloSommaR(T, l, s, 0, 0) >= s
 
 def livelloSommaR(T, l, s, distance, somma):
     if l == distance:
         somma += t.value(T)
-        if somma > s:
-            return True # assumo valori interi positivi
     elif distance < l:
-        left = right = False
+        left = right = 0
         if not t.empty(t.right(T)):
             right = livelloSommaR(t.right(T), l, s, distance + 1, somma)
         if not t.empty(t.left(T)):
             left = livelloSommaR(t.left(T), l, s, distance + 1, somma)
-        return right or left
-    return False
+        return right + left
+    return somma
 
 A = t.createTree()
 s = [1, 2, 3, 4, 5, 6, 7, 8, 9]
