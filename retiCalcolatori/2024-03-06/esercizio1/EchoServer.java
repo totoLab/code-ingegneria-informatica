1e9f85cd4d7c7252d5180cc7ab354a96c84fdf65
toto <antoniolabate19@gmail.com>
2024-03-09 12:43:55 +0100

    esercizio per casa

---
+++ retiCalcolatori/2024-03-06/esercizio1/EchoServer.java
@@ -0,0 +1,37 @@
+package esercitazione1;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class EchoServer {
+    public static void main(String[] args ) {
+        try {
+            ServerSocket s = new ServerSocket(8189);
+            while (true) {
+                System.out.println("Waiting for a connection...");
+                Socket incoming = s.accept(); // stop in attesa di una connessione
+                System.out.println("Connected to " + incoming.getInetAddress() + ":" + incoming.getPort());
+                BufferedReader in = new BufferedReader
+                        (new InputStreamReader(incoming.getInputStream()));
+                PrintWriter out = new PrintWriter
+                        (incoming.getOutputStream(), true /* autoFlush */);
+                out.println("Hello! Enter BYE to exit.");
+                boolean done = false;
+                while (!done) {
+                    String line = in.readLine();
+                    System.out.println("Received: " + line);
+
+                    if (line == null)
+                        done = true;
+                    else {
+                        out.println("Echo: " + line);
+                        if (line.trim().equals("BYE"))
+                            done = true;
+                    }
+                }
+                incoming.close();
+            }
+        } catch (Exception e) { System.err.println(e); }
+    }
+}
---
+++ retiCalcolatori/2024-03-06/esercizio1/EchoServerClient.java
@@ -0,0 +1,55 @@
+package esercitazione1;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoServerClient {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Socket socket = null;
+        int port = 8189;
+        try {
+            InetAddress server = InetAddress.getLocalHost();
+            socket = new Socket(server, port);
+
+            boolean done = false;
+            while (!done) {
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                                socket.getInputStream()
+                        )
+                );
+                String response = in.readLine();
+                System.out.println(response);
+
+                String userInput = s.next();
+                String message = userInput;
+                if (userInput.equals("BYE")) {
+                    done = true;
+                } else if (userInput.equals("EXIT")) {
+                    if (!socket.isClosed()) {
+                        message = "BYE";
+                    }
+                    done = true;
+                }
+                sendToSocket(socket, message);
+
+                if (done) {
+                    socket.close();
+                    return;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void sendToSocket(Socket socket, String message) throws IOException {
+        PrintWriter pw = new PrintWriter(
+                socket.getOutputStream(), true
+        );
+        pw.println(message);
+    }
+}
