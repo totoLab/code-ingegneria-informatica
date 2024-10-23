937fff7f3ab0d3e307e1151b4bc750b126417075
toto <antoniolabate19@gmail.com>
2024-10-23 11:32:02 +0200

    test implementazione

--- fondamentiDiInformatica1/argomenti/listeTuple/esercizi/eratosteneSieve.py
+++ fondamentiDiInformatica1/argomenti/listeTuple/esercizi/eratosteneSieve.py
@@ -1,15 +1,45 @@
-def sieve(divisor, array):
-    for number in array:
-        if number != divisor and number % divisor == 0:
-            array.pop(array.index(number))
+def sieve(divisor_index, numbers):
+    # Convert keys to a list and get the current divisor
+    keys_list = list(numbers.keys())
+    divisor = keys_list[divisor_index]
+    
+    # Iterate through the numbers and remove non-primes
+    for number in keys_list[divisor_index + 1:]:
+        if number % divisor == 0:
+            numbers.pop(number)
 
-    #print("Divisor: {}, state of list: {}".format(divisor, array))
-    if array[len(array) - 1] != divisor:
-        newDivisor = array[array.index(divisor) + 1]
-        sieve(newDivisor, array)
+    # Recursively call sieve until we have processed all numbers
+    if divisor_index < len(numbers) - 1:
+        sieve(divisor_index + 1, numbers)
     else:
-        print("First {} prime numbers: {}".format(len(array), array))
+        print("Prime numbers:", list(numbers.keys()))
 
-n = int(input("Last number of the sequence to generate? "))
-array = list(range(2, n))
-sieve(array[0], array)
+def sieveI(numbers: list):
+    index = 0
+    while index < len(numbers) - 1:
+        divisor = numbers[index]
+        for i, number in reversed(list(enumerate(numbers[index + 1:]))):
+            if number % divisor == 0:
+                numbers.pop(index + 1 + i)
+        index += 1
+
+    print("Prime numbers:", numbers)
+
+def sieveI_optimized(numbers):
+    index = 0
+    while index < len(numbers) - 1:
+        divisor = numbers[index]
+        i = index + 1
+        while i < len(numbers):
+            if numbers[i] % divisor == 0:
+                numbers.pop(i)
+            else:
+                i += 1
+        index += 1
+
+    print("Prime numbers:", numbers)
+
+n = 10 ** 8 # int(input("Last number of the sequence to generate? "))
+numbers = {number: None for number in range(2, n + 1)}
+
+sieveI_optimized(list(range(1, n + 1, 2))[1:])
