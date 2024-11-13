d15566f3e20188c628f30a70043661c31f3666d4
toto <antoniolabate19@gmail.com>
2024-11-13 13:12:24 +0100

    ConcurrentModificationException easiest case

--- sistemiOperativi/esercitazioni/src/test/ListTest.java
+++ sistemiOperativi/esercitazioni/src/test/ListTest.java
@@ -1,4 +1,21 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class ListTest {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>( Arrays.asList(1, 2, 3, 4, 5, 6) );
+        Iterator<Integer> iterator = list.iterator();
+
+        for (int i = 0; i < list.size() / 2; i++) {
+            iterator.next();
+        }
+        list.add(7);
+        iterator.next();
+    }
 }
